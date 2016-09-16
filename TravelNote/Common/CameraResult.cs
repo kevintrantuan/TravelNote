348bc8f697710de88fd82dd3940d4fb6f8bb8035
Duc phat <phampeter620@gmail.com>
2016-09-16 14:55:14 +1000

    navi2

---
+++ TravelNote/Common/CameraResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TravelNote
+{
+	public class CameraResult
+	{
+		public CameraResult()
+		{
+		}
+	}
+}
+
---
+++ TravelNote/Common/ICameraProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TravelNote
+{
+	public class ICameraProvider
+	{
+		public ICameraProvider()
+		{
+		}
+	}
+}
+
